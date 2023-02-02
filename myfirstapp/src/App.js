ddede88f0653a2602d18d6cbf73109c5e3cfd0f1
Srishti Mishra <srishtimishra5672@gmail.com>
2023-02-02 12:36:11 +0530

    added date and time

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+function App(props) {
+  const currDate = new Date();
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Srishti Mishra</h1>
+      <h2>Today Date is {currDate.toLocaleDateString()}.</h2>
+      <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
     </div>
   );
 }
